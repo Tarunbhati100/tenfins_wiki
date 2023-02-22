68d2f919e97e64bddf6dd6a77e7552377d1c76bb
Nitin Bhalala <bhalalanitin5291@gmail.com>
2023-02-22 18:21:12 +0530

    Demo

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import 'package:sizer/sizer.dart';
+import 'package:tenfins_wiki/splashscreen.dart';
+import 'common/color.dart';
+
+void main()async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool loginstatus = false;
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Html Editor App',
+        theme: ThemeData(
+          canvasColor: AppColor.whiteColor,
+          primaryColor: AppColor.primary,
+        ),
+        home: const SplashScreen(),
+      );
+    },);
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,82 @@
+// import 'package:dna/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:tenfins_wiki/html_editor_page.dart';
+import '../../common/widget.dart';
+import 'common/color.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (mounted) => htmlEditorPage()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    //...
+    super.dispose();
+    //...
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: _buildbody(),
+    );
+  }
+
+  Widget _buildbody() {
+    var height = SizeConfig.getHeight(context);
+    var width = SizeConfig.getWidth(context);
+    return SafeArea(
+      top: false,
+      child: Stack(
+        children: [
+          Image.asset(
+            "assets/images/splash.png",
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    SizedBox(height: height * 0.05),
+                    Image.asset(
+                      'assets/images/coding.png',
+                      height: height * 0.3,
+                      width: width * 0.5,
+                    ),
+                    appText(
+                        title: 'HTML Editor App',
+                        fontWeight: FontWeight.w700,
+                        color: AppColor.whiteColor,
+                        fontSize: 20)
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
