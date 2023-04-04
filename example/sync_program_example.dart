b3f44703a78e5eb515ddb4d86bd7f8d5aa75b087
Nitin Bhalala <bhalalanitin5291@gmail.com>
2023-04-04 17:18:30 +0530

    sync program update

---
+++ example/sync_program_example.dart
@@ -0,0 +1,6 @@
+import 'package:sync_program/sync_program.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/sync_program.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library sync_program;
+
+export 'src/sync_program_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
